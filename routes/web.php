a0875e85bc3f3d73eb85f01151f30c4cd41a36e7
AldamaitaSS <pijarheksa02@gmail.com>
2024-09-04 09:25:17 +0700

    Jobsheet 2_Praktikum1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,50 @@
 
 use Illuminate\Support\Facades\Route;
 
+// PRAKTIKUM 1
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/SelamatDatang', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241760050 Aldamaita';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articels/{id}', function ($Id) {
+    return 'Halaman Artikel dengan ID '.$Id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+// PRAKTIKUM 2
+
+   
 /*
 |--------------------------------------------------------------------------
 | Web Routes
